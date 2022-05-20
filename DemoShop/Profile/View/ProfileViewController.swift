75706a1723c413fb84d2b975690ee1c48a5d2f63
leo <a841223o@gmail.com>
2022-05-20 12:11:05 +0800

    setup a table in profile

--- DemoShop/Profile/View/ProfileViewController.swift
+++ DemoShop/Profile/View/ProfileViewController.swift
@@ -9,13 +9,41 @@ import Foundation
 import UIKit
 
 
-class ProfileViewController : UIViewController {
+class ProfileViewController : UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    let tableView : UITableView = UITableView(frame: CGRect.zero, style: .insetGrouped)
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "查看訂單"
+        cell.imageView?.image = UIImage.init(systemName: "list.bullet.rectangle.portrait")
+        cell.imageView?.contentMode = .scaleAspectFit
+        cell.imageView?.tintColor = .systemRed
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+    
     
     override func viewDidLoad() {
         self.view.backgroundColor = .white
         setupNavigationBar()
+        setupTableView()
     }
-    
+    func setupTableView(){
+        self.view.addSubview(tableView)
+        tableView.frame.size = self.view.frame.size
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
     func setupNavigationBar(){
        
         let appearance = UINavigationBarAppearance.init()
@@ -26,4 +54,6 @@ class ProfileViewController : UIViewController {
         navigationItem.title = "Profile"
         
     }
+    
+    
 }
